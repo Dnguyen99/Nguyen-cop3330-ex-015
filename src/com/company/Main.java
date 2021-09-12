febe2a703281faecf36eba0dd5cea39cd557f53c
Dnguyen99 <dylannguyen@Knights.ucf.edu>
2021-09-12 09:16:08 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+        String kp = "abc$123";
+        System.out.print("Enter your username: ");
+        String user = scan.nextLine();
+        System.out.print("What is the password? ");
+        String pass = scan.nextLine();
+
+        if(kp.equals(pass)) {
+            System.out.println("Welcome!");
+        }
+        else {
+            System.out.println("I don't know you.");
+        }
+    }
+}
